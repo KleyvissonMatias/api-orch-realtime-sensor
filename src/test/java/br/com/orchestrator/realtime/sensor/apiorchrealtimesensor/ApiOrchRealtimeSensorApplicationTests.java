5852fb6f7a93e3bbb81254bd8d402266d68f3c80
Kley <kleyvissonmatias@gmail.com>
2024-03-11 15:36:44 -0300

    Initial Commit

---
+++ src/test/java/br/com/orchestrator/realtime/sensor/apiorchrealtimesensor/ApiOrchRealtimeSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orchestrator.realtime.sensor.apiorchrealtimesensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrchRealtimeSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
